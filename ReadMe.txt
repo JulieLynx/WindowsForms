1113ec003f094144290d98ef7374904e6b4eab86
Clayman <Clayman@DESKTOP-DM8E77U>
2020-04-21 21:27:59 +0300

    ComboBox DONE.

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,4 +1,13 @@
 https://github.com/okovtun/WindowsApplications34v
 
 TODO:
+1. MessageBox с выбранным пунктом ComboBox, должен появляться при нажатии на кнопку OK, а не при выборе;
+2. Создать диалоговое окно с элементом ListBox, добавить в него те же пункты (строки) и выбранну строку отобразить в MessageBox при нажатии на кнопку OK. Работа с ListBox ОЧЕНЬ ПОХОЖА на ComboBox;
+
+https://www.youtube.com/watch?v=18c3MTX0PK0&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=1
+https://www.youtube.com/watch?v=SfGuIVzE_Os&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=5
+https://www.youtube.com/watch?v=3tIqpEmWMLI&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=6
+https://www.youtube.com/watch?v=H4s55GgAg0I&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=7
+
+DONE:
 В текстовое поле вводится имя пользователя, и после нажатия на Enter появляется MessageBox, в котором программа приветствует пользователя, например: "Привет Вася ;-).";
--- WinAPI/ComboBox/main.cpp
+++ WinAPI/ComboBox/main.cpp
@@ -1,6 +1,20 @@
 #include<Windows.h>
 #include"resource.h"
 
+HWND hCombo;
+
+/*CONST CHAR str1[] = "This";
+CONST CHAR str2[] = "is";
+CONST CHAR str3[] = "my";
+CONST CHAR str4[] = "first";
+CONST CHAR str5[] = "Combo";
+CONST CHAR str6[] = "Box";*/
+
+CONST CHAR* str[] =
+{
+	"This","is","my","first", "Combo", "Box"
+};
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
@@ -14,10 +28,44 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
-		break;
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+		for (int i = 0; i < sizeof(str)/sizeof(*str) ; i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)str[i]);
+		}
+
+		/*SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)str1);
+		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)str2);
+		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)str3);
+		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)str4);
+		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)str5);
+		SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)str6);*/
+
+		SendMessage(hCombo, CB_SETCURSEL, 1, 0);
+	}
+	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_COMBO1:
+		{
+			if (HIWORD(wParam) == CBN_SELENDOK)
+			{
+				const int SIZE = 255;
+				CHAR buffer[SIZE]{};
+				CHAR message[SIZE] = "Вы выбрали: ";
+				int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)buffer);
+
+				strcat_s(message, SIZE, buffer);
+				MessageBox(hwnd, message, "Info", MB_OK | MB_ICONINFORMATION);
+			}
+		}
+		break;
 		case IDOK:
 			MessageBox(hwnd, "Hello", "Hi", MB_OK);
 			break;
