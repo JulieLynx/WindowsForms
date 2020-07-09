c7868802fa588edf14381691eb48cd904e15e1b2
yulie <yulie@DESKTOP-7A64TCF>
2020-07-09 16:36:49 +0300

    cancel_backs_+-*/_pown

--- WinAPI/Calc/main.cpp
+++ WinAPI/Calc/main.cpp
@@ -2,20 +2,21 @@
 #include<string>
 #include"resource.h"
 
-//HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
-double a = 0;
-double b = 0;
-CHAR buffer[256]{};
+CONST UINT N_BUFFER_SIZE = 256;
+CHAR sz_buffer[N_BUFFER_SIZE]{};
+CHAR sz_buffer_a[N_BUFFER_SIZE]{};
+CHAR sz_buffer_b[N_BUFFER_SIZE]{};
+double a = 0, b = 0;
 CHAR s;
 
+HWND hEdit;
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-VOID Count(HWND hEdit, double& a, char s, double& b);
-
+VOID GetNumber();
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc, 0);
 	return 0;
 }
 
@@ -24,81 +25,126 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
-
-		break;
+	{
+		hEdit = GetDlgItem(hwnd, IDC_EDIT1);
+		SetFocus(hEdit);
+	}
+	break;
 	case WM_COMMAND:
 	{
-		switch (LOWORD(wParam))
+		if (LOWORD(wParam) >= 1000 && LOWORD(wParam) <= 1009)
 		{
-		case IDC_BUTTON0:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x30, 0); break;
-		case IDC_BUTTON1:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x31, 0); break;
-		case IDC_BUTTON2:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x32, 0); break;
-		case IDC_BUTTON3:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x33, 0); break;
-		case IDC_BUTTON4:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x34, 0); break;
-		case IDC_BUTTON5:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x35, 0); break;
-		case IDC_BUTTON6:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x36, 0); break;
-		case IDC_BUTTON7:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x37, 0); break;
-		case IDC_BUTTON8:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x38, 0); break;
+			GetWindowText(hEdit, sz_buffer, N_BUFFER_SIZE);
+			if (strcmp(sz_buffer, sz_buffer_a) == 0)
+			{
+				SetWindowText(hEdit, "");
+			}
+			SendMessage(hEdit, WM_CHAR, LOWORD(wParam) - 952, 0);
+		}
+		switch (LOWORD(wParam))
+		{/*
+		case IDC_BUTTON(0-9):SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 0x3(0-9), 0); break;*/
 		case IDC_BUTTON_POINT:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 46, 0); break;
+		case IDC_BUTTON_BACK:SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_CHAR, 8, 0); break;
+		case IDC_BUTTON_CLEAR:
+		{
+			//LPCSTR tx = "";
+			HWND   hwndEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
+			SetWindowText(hwndEdit1, "");
+			a = b = 0;
+			//SendMessage(GetDlgItem(hwnd, IDC_EDIT1),  = ""); break;
+		}
+		case IDC_BUTTON_SQRT:
+		{
+			if (s)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_RESULT, 0);
+			s = 'pow2';
+		}
+		break;
+		case IDC_BUTTON_POW2:
+		{
+			if (s)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_RESULT, 0);
+			s = 'pow2';
+		
+		}
+		break;
+		case IDC_BUTTON_POWN:
+		{
+			if (s)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_RESULT, 0);
+			s = 'pown';
+			GetNumber();
+		}
+		break;
+#pragma region sign
 
 		case IDC_BUTTON_PLUS:
-		{
-
-			s = '+';
-			GetNumber();
-
+		{ if (s)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_RESULT, 0);
+		s = '+';
+		GetNumber();
 		}
 		break;
+		case IDC_BUTTON_MINUS:
+		{ if (s)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_RESULT, 0);
+		s = '-';
+		GetNumber();
+		}
+		break;
+		case IDC_BUTTON_ASTER:
+		{ if (s)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_RESULT, 0);
+		s = '*';
+		GetNumber();
+		}
+		case IDC_BUTTON_SLASH:
+		{ if (s)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_RESULT, 0);
+		s = '/';
+		GetNumber();
+		}
+		break;
+#pragma endregion
+
 		case IDC_BUTTON_RESULT:
-		{		GetNumber();
-		switch (s)
 		{
-		case '+': a += b; break;
-		case '-': a -= b; break;
-		case '*': a *= b; break;
-		case '/': a /= b; break;
+			GetNumber();
+			switch (s)
+			{
+			case '+': a += b; break;
+			case '-': a -= b; break;
+			case '*': a *= b; break;
+			case '/': a /= b; break;
+			case 'pown':
+			{
+				int buf = a;
+				for (int i = 0; i < b-1; i++) { buf *= a; }
+				a = buf;
+			}
+			break;
+			}
+			b == 0;
+			s = 0;
+			strcpy(sz_buffer_a, std::to_string(a).c_str());
+			SetWindowText(hEdit, sz_buffer_a);
 		}
-		b == 0;
-		s = 0;
-		strcpy(sz_buffer_a, std::to_string(a).)
 		break;
-
 		}
-		}
-		break;
+	}
+	break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 		break;
 	}
-
-	}
 	return FALSE;
 }
 
-VOID Count(HWND hEdit, double& a, char s, double& b)
+VOID GetNumber()
 {
-	GetWindowText(hEdit, buffer, 256);
+	GetWindowText(hEdit, sz_buffer, N_BUFFER_SIZE);
 	if (a == 0)
 	{
-		a = atof(buffer);
+		strcpy(sz_buffer_a, sz_buffer);
+		a = atof(sz_buffer);
 	}
 	else
 	{
-		b = atof(buffer);
-		a += b;
-		strcpy(buffer, std::to_string(a).c_str());
-		SetWindowText(hEdit, buffer);
+		strcpy(sz_buffer_b, sz_buffer);
+		b = atof(sz_buffer);
 	}
-	if (b == 0)SetWindowText(hEdit, "");
-	switch (s)
-	{
-	case '+':a += b; break;
-	case '-':a -= b; break;
-	case '*':a *= b; break;
-	case '/':a /= b; break;
-	}
-}
-VOID GetNumber() 
-{
-	GetWindowText(hEdit, sz_buffer, N_BUFFER_SIZE;)
 }
--- WinAPI/Calc/resource.h
+++ WinAPI/Calc/resource.h
@@ -1,27 +1,30 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by Calc.rc
+// Used by Resource.rc
 //
 #define IDD_DIALOG1                     101
-#define IDC_EDIT1                       1001
-#define IDC_BUTTON7                     1003
-#define IDC_BUTTON8                     1004
-#define IDC_BUTTON9                     1005
-#define IDC_BUTTON4                     1006
-#define IDC_BUTTON5                     1007
-#define IDC_BUTTON6                     1008
-#define IDC_BUTTON1                     1009
-#define IDC_BUTTON2                     1010
-#define IDC_BUTTON3                     1011
-#define IDC_BUTTON0                     1012
-#define IDC_BUTTON11                    1013
-#define IDC_BUTTON_RESULT               1013
+#define IDC_EDIT1                       999
+#define IDC_BUTTON0                     1000
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define IDC_BUTTON3                     1003
+#define IDC_BUTTON4                     1004
+#define IDC_BUTTON5                     1005
+#define IDC_BUTTON6                     1006
+#define IDC_BUTTON7                     1007
+#define IDC_BUTTON8                     1008
+#define IDC_BUTTON9                     1009
+#define IDC_BUTTON_POINT                1010
 #define IDC_BUTTON_PLUS                 1014
 #define IDC_BUTTON_MINUS                1015
-#define IDC_BUTTON14                    1016
-#define IDC_BUTTON15                    1017
-#define IDC_BUTTON_POINT                1018
-#define IDC_BUTTON17                    1019
+#define IDC_BUTTON_ASTER                1016
+#define IDC_BUTTON_SLASH                1017
+#define IDC_BUTTON_RESULT               1018
+#define IDC_BUTTON_CLEAR                1019
+#define IDC_BUTTON_SQRT                 1020
+#define IDC_BUTTON_BACK                 1021
+#define IDC_BUTTON_POW2                 1022
+#define IDC_BUTTON_POWN                 1023
 
 // Next default values for new objects
 // 
@@ -29,7 +32,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1020
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
